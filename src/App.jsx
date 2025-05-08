5321996f2a0f996c6927140c872068ec2c7071a7
Andrea-Sgura <andreasgura96@gmail.com>
2025-05-09 01:07:17 +0200

    [UPD] Creato file Main.jsx + importato in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import Header from "./Header"
+import Header from "./components/Header"
+import Main from "./components/Main"
 
 function App() {
   return (
     <>
       <Header />
-      <button>Click me</button>
+      <Main />
     </>
   )
 }
