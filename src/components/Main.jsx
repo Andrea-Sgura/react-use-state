c4002972fcd6204834f9ec0043e076ff06e2b142
Andrea-Sgura <andreasgura96@gmail.com>
2025-05-09 02:14:29 +0200

    [UPD] Creato file Languages.jsx, creata struttura, importato in Main.jsx e creata struttura

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import Languages from './Languages'
 
 const Main = () => {
     return (
-        <button>Click me</button>
+        <>
+            <div className="container">
+                <button className="btn">
+                    <h4 className="btn-title"></h4>
+                </button>
+            </div>
+            <Languages />
+        </>
     )
 }
 
